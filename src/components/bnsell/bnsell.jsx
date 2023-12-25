30be79b92a97b29fdb1ba1525afbd610e00517f1
samstickkz <samstickkz@gmail.com>
2023-12-25 12:37:13 +0100

    coins page

--- src/components/bnsell/bnsell.jsx
+++ src/components/bnsell/bnsell.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import BTC from "./../../assets/coin/btc.png";
+import ETH from "./../../assets/coin/eth.png";
+import LTC from "./../../assets/coin/ltc.png";
+import XRP from "./../../assets/coin/xrp.png";
+import BCH from "./../../assets/coin/bch.png";
+import BNB from "./../../assets/coin/bnb.png";
+import EOS from "./../../assets/coin/eos.png";
+import XLM from "./../../assets/coin/xlm.png";
+import TRX from "./../../assets/coin/trx.png";
+import ADA from "./../../assets/coin/ada.png";
+import XMR from "./../../assets/coin/xmr.png";
+import DASH from "./../../assets/coin/dash.png";
+
 import Bns from "./bns.css";
+
 function bnsell() {
   return (
     <section>
@@ -15,44 +28,34 @@ function bnsell() {
             <small className="sbg">$12,000</small>
           </div>
           <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
+            <img src={ETH} className="cnsize" />
             <p>Bitcoin (BTC)</p>
             <small className="sbg">$12,000</small>
           </div>
           <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
+            <img src={LTC} className="cnsize" />
             <p>Bitcoin (BTC)</p>
             <smal className="sbg" l>
               $12,000
             </smal>
           </div>
           <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
+            <img src={XRP} className="cnsize" />
             <p>Bitcoin (BTC)</p>
             <small className="sbg">$12,000</small>
           </div>
           <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
-            <p>Bitcoin (BTC)</p>
-            <small>$12,000</small>
-          </div>
-          <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
+            <img src={BCH} className="cnsize" />
             <p>Bitcoin (BTC)</p>
             <small className="sbg">$12,000</small>
           </div>
           <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
+            <img src={BNB} className="cnsize" />
             <p>Bitcoin (BTC)</p>
             <small className="sbg">$12,000</small>
           </div>
           <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
-            <p>Bitcoin (BTC)</p>
-            <small className="sbg">$12,000</small>
-          </div>
-          <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
+            <img src={EOS} className="cnsize" />
             <p>Bitcoin (BTC)</p>
             <small className="sbg">$12,000</small>
           </div>
@@ -72,17 +75,27 @@ function bnsell() {
             <small className="sbg">$12,000</small>
           </div>
           <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
+            <img src={XLM} className="cnsize" />
             <p>Bitcoin (BTC)</p>
             <small className="sbg">$12,000</small>
           </div>
           <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
+            <img src={TRX} className="cnsize" />
             <p>Bitcoin (BTC)</p>
             <small className="sbg">$12,000</small>
           </div>
           <div className="coinsgrp">
-            <img src={BTC} className="cnsize" />
+            <img src={DASH} className="cnsize" />
+            <p>Bitcoin (BTC)</p>
+            <small className="sbg">$12,000</small>
+          </div>
+          <div className="coinsgrp">
+            <img src={XMR} className="cnsize" />
+            <p>Bitcoin (BTC)</p>
+            <small className="sbg">$12,000</small>
+          </div>
+          <div className="coinsgrp">
+            <img src={ADA} className="cnsize" />
             <p>Bitcoin (BTC)</p>
             <small className="sbg">$12,000</small>
           </div>
