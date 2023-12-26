a7b81943353938491193af64e37e7aed5857a4e0
samstickkz <samstickkz@gmail.com>
2023-12-26 12:38:14 +0100

    fix

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -10,63 +10,60 @@ import { IoLogoYoutube } from "react-icons/io5";
 
 function footer() {
   return (
-    <footer className="footer">
-      <div className="column">
-        <LazyLoadImage
-          src={Logo}
-          alt="CoinBubble Logo"
-          className="logo"
-          effect="blur"
-        />
-        Lorem ipsum dolor sit amet consectetur adipiscing elit aliquam
-        <div className="sub-logos">
-          {/* Additional CoinBubble logos can be added here */}
-          <small>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit aliquam
-          </small>
-          <div>
-            <FaFacebook />
-            <FaTwitter />
-            <FaInstagramSquare />
-            <FaLinkedin />
-            <IoLogoYoutube />
+    <section>
+      <footer className="footer">
+        <div className="column">
+          <img src={Logo} alt="CoinBubble Logo" className="logo" />
+
+          <div className="sub-logos">
+            {/* Additional CoinBubble logos can be added here */}
+            <small className="ftsmall">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit aliquam
+            </small>
+            <div className="ftlogo">
+              <FaFacebook className="lgn" />
+              <FaTwitter className="lgn" />
+              <FaInstagramSquare className="lgn" />
+              <FaLinkedin />
+              <IoLogoYoutube className="lgn" />
+            </div>
+            {/* Add more as needed */}
           </div>
-          {/* Add more as needed */}
         </div>
-      </div>
 
-      <div className="column">
-        <h4>Product</h4>
-        <ul>
-          <li>Buy & Sell</li>
-          <li>Cards</li>
-          {/* Add more features as needed */}
-        </ul>
-      </div>
+        <div className="column">
+          <h4>Product</h4>
+          <ul>
+            <li>Buy & Sell</li>
+            <li>Cards</li>
+            {/* Add more features as needed */}
+          </ul>
+        </div>
 
-      <div className="column">
-        <h4>Company</h4>
-        <ul>
-          <li>About Us</li>
-          <li>Team</li>
-          {/* Add more company-related links as needed */}
-        </ul>
-      </div>
+        <div className="column">
+          <h4>Company</h4>
+          <ul>
+            <li>About Us</li>
+            <li>Team</li>
+            {/* Add more company-related links as needed */}
+          </ul>
+        </div>
 
-      <div className="column">
-        <h4>Support</h4>
-        <ul>
-          <li>Contact Us</li>
-          <li>FAQs</li>
-          {/* Add more support-related links as needed */}
-        </ul>
-      </div>
+        <div className="column">
+          <h4>Support</h4>
+          <ul>
+            <li>Contact Us</li>
+            <li>FAQs</li>
+            {/* Add more support-related links as needed */}
+          </ul>
+        </div>
 
-      <div className="column">
-        <h4>Footer</h4>
-        <p>© 2023 Your Company Name. All Rights Reserved.</p>
-      </div>
-    </footer>
+        <div className="column">
+          <h4>Footer</h4>
+          <p>© 2023 Your Company Name. All Rights Reserved.</p>
+        </div>
+      </footer>
+    </section>
   );
 }
 
