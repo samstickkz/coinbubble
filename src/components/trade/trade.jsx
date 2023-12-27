eec1c1f70ef4a41118644c3c1036422b5b68d28d
samstickkz <samstickkz@gmail.com>
2023-12-27 16:03:39 +0100

    fixes

--- src/components/trade/trade.jsx
+++ src/components/trade/trade.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Trades from "./trades.css";
 import { FaCheckCircle } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa6";
 
 function trade() {
   return (
@@ -14,39 +15,45 @@ function trade() {
         <div className="trade_down">
           {" "}
           <div className="td_row">
-            <FaCheckCircle />
+            <div className="ch">
+              <FaCheck className="chb" />
+            </div>
 
             <div className="para">
               <h4 className="h4t">Buy and sell crypto in minutes</h4>
-              <p className="para ptext">
+              <small className="para ptext">
                 Lorem ipsum dolor sit amet consectetur.
                 <br /> At cursus diam sagittis maecenas. Aliquam <br />{" "}
                 ultricies tristique augue
-              </p>
+              </small>
             </div>
           </div>
           <div className="td_row">
-            <FaCheckCircle />
+            <div className="ch">
+              <FaCheck className="chb" />
+            </div>
 
             <div className="para">
               <h4 className="h4t">Buy and sell crypto in minutes</h4>
-              <p className="para ptext">
+              <small className="para ptext">
                 Lorem ipsum dolor sit amet consectetur.
                 <br /> At cursus diam sagittis maecenas. Aliquam <br />{" "}
                 ultricies tristique augue
-              </p>
+              </small>
             </div>
           </div>
           <div className="td_row">
-            <FaCheckCircle />
+            <div className="ch">
+              <FaCheck className="chb" />
+            </div>
 
             <div className="para">
               <h4 className="h4t">Buy and sell crypto in minutes</h4>
-              <p className="para ptext">
+              <small className="para ptext">
                 Lorem ipsum dolor sit amet consectetur.
                 <br /> At cursus diam sagittis maecenas. Aliquam <br />{" "}
                 ultricies tristique augue
-              </p>
+              </small>
             </div>
           </div>
         </div>
